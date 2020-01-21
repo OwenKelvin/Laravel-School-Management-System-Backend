bd61a7c8319ca771078381d1bf8e27e5673848ab
Owen Kelvin <otienoowenkelvin@gmail.com>
2020-01-21 06:04:59 +0300

    Added library classes routes

---
+++ packages/okotieno/laravel-lms/src/Controllers/LibraryClassificationClassController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: oko
+ * Date: 1/20/2020
+ * Time: 10:07 PM
+ */
+
+namespace Okotieno\LMS\Controllers;
+
+
+class LibraryClassificationClassController
+{
+
+}
