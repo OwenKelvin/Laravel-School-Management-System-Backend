9fbdee9545ee70ee56545b0498ecd6f51a2a6f5a
Owen Kelvin <otienoowenkelvin@gmail.com>
2020-06-12 20:43:45 +0300

    Added religion in user profile

--- packages/okotieno/religion/src/controllers/ReligionController.php
+++ packages/okotieno/religion/src/controllers/ReligionController.php
@@ -4,8 +4,11 @@ namespace Okotieno\Religion\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Okotieno\Religion\Models\Religion;
 
 class ReligionController extends Controller
 {
-    //
+    public function index() {
+        return response()->json(Religion::all());
+    }
 }
--- packages/okotieno/religion/src/routes.php
+++ packages/okotieno/religion/src/routes.php
@@ -2,6 +2,7 @@
 
 Route::middleware(['auth:api', 'bindings'])->group(function () {
 
+    Route::resource('/api/religions', 'Okotieno\\Religion\\Controllers\\ReligionController');
     Route::get('/api/religions/all', 'Okotieno\\Religion\\Controllers\\ReligionApiController@getAll');
 
 });
