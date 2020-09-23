e788047574d252958486b77ac67b1eb9a3e92949
Owen Kelvin <otienoowenkelvin@gmail.com>
2020-09-23 06:56:48 +0300

    Fixed error in publisher

--- packages/okotieno/laravel-lms/src/Controllers/LibraryBookPublisherController.php
+++ packages/okotieno/laravel-lms/src/Controllers/LibraryBookPublisherController.php
@@ -12,121 +12,120 @@ use Okotieno\LMS\Request\StoreLibraryBookPublisherRequest;
 
 class LibraryBookPublisherController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    return view('okotieno.lms.books.add_books');
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param StoreLibraryBookRequest $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(StoreLibraryBookPublisherRequest $request)
+  {
+    $saved_picture = null;
+    if ($request->profilePicture !== null) {
+      $filePath = Storage::put('uploads/library_publishers', $request->profilePicture);
+      $saved_picture = User::find(auth()->id())->uploadFileDocument()->create([
+        'name' => $request->profilePicture->getClientOriginalName(),
+        'type' => $request->profilePicture->getClientOriginalExtension(),
+        'extension' => $request->profilePicture->getClientOriginalExtension(),
+        'mme_type' => $request->profilePicture->getMimeType(),
+        'size' => $request->profilePicture->getSize(),
+        'file_path' => $filePath
+      ]);
 
     }
+    $createdPublisher = LibraryBookPublisher::create([
+      'name' => $request->name,
+      'biography' => $request->biography
+    ]);
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('okotieno.lms.books.add_books');
+
+    if ($saved_picture != null) {
+      $profilePic = ProfilePic::create([
+        'file_document_id' => $saved_picture->id,
+        'user_id' => $createdPublisher->id
+      ]);
+      $createdPublisher->profilePics()->save($profilePic);
     }
+    return response()->json([
+      'saved' => true,
+      'message' => 'Publisher saved Successfully',
+      'data' => $createdPublisher
+    ]);
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param StoreLibraryBookRequest $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreLibraryBookPublisherRequest $request)
-    {
+  /**
+   * Display the specified resource.
+   *
+   * @param LibraryBookPublisher $libraryBookPublisher
+   * @return \Illuminate\Http\Response
+   */
+  public function show(LibraryBookPublisher $libraryBookPublisher)
+  {
+    return response()->json($libraryBookPublisher);
+  }
 
-        if ($request->profilePicture !== null) {
-            $filePath = Storage::put('uploads/library_publishers', $request->profilePicture);
-            $saved_picture = User::find(auth()->id())->uploadFileDocument()->create([
-                'name' => $request->profilePicture->getClientOriginalName(),
-                'type' => $request->profilePicture->getClientOriginalExtension(),
-                'extension' => $request->profilePicture->getClientOriginalExtension(),
-                'mme_type' => $request->profilePicture->getMimeType(),
-                'size' => $request->profilePicture->getSize(),
-                'file_path' => $filePath
-            ]);
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param int $id
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+    //
+  }
 
-        }
-        $createdPublisher = LibraryBookPublisher::create([
-            'name' => $request->name,
-            'biography' => $request->biography
-        ]);
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param \Illuminate\Http\Request $request
+   * @param LibraryBookPublisher $libraryBookPublisher
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, LibraryBookPublisher $libraryBookPublisher)
+  {
 
+    $libraryBookPublisher->update(['name' => $request->name, 'biography' => $request->biography]);
+    return response()->json([
+      'saved' => true,
+      'message' => 'Publisher Updated Successfully',
+      'data' => LibraryBookPublisher::find($libraryBookPublisher->id)
+    ]);
+  }
 
-
-        if ($saved_picture != null) {
-            $profilePic = ProfilePic::create([
-                'file_document_id' => $saved_picture->id,
-                'user_id' => $createdPublisher->id
-            ]);
-            $createdPublisher->profilePics()->save($profilePic);
-        }
-        return response()->json([
-            'saved' => true,
-            'message' => 'Publisher saved Successfully',
-            'data' => $createdPublisher
-        ]);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param LibraryBookPublisher $libraryBookPublisher
-     * @return \Illuminate\Http\Response
-     */
-    public function show(LibraryBookPublisher $libraryBookPublisher)
-    {
-        return response()->json($libraryBookPublisher);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param LibraryBookPublisher $libraryBookPublisher
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, LibraryBookPublisher $libraryBookPublisher)
-    {
-
-        $libraryBookPublisher->update(['name' => $request->name, 'biography' => $request->biography]);
-        return response()->json([
-            'saved' => true,
-            'message' => 'Publisher Updated Successfully',
-            'data' => LibraryBookPublisher::find($libraryBookPublisher->id)
-        ]);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param LibraryBookPublisher $libraryBookPublisher
-     * @return \Illuminate\Http\Response
-     * @throws \Exception
-     */
-    public function destroy(LibraryBookPublisher $libraryBookPublisher)
-    {
-        $libraryBookPublisher->delete();
-        return response()->json([
-            'saved' => true,
-            'message' => 'Publisher Updated Successfully',
-        ]);
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param LibraryBookPublisher $libraryBookPublisher
+   * @return \Illuminate\Http\Response
+   * @throws \Exception
+   */
+  public function destroy(LibraryBookPublisher $libraryBookPublisher)
+  {
+    $libraryBookPublisher->delete();
+    return response()->json([
+      'saved' => true,
+      'message' => 'Publisher Updated Successfully',
+    ]);
+  }
 }
